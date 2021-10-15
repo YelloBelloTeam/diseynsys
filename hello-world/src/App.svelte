7ad8de8fe626c8f41264edb8a0855373f225f7bb
vhollo <vhollo@protonmail.com>
2021-10-15 15:05:55 +0000

    Push from main

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -9,4 +9,10 @@
 <main>
   <SwitchTheme />
   <h1>Hello {name? name : 'World'}!</h1>
+  <h2>What is perceptualism?</h2>
+  <p>These are not memory patterns of the self, rather the perception itself.</p>
+  <h2>Who owns the politicians?</h2>
+  <p>Politics by design is our service. As politicians work for the only purpose of this service.</p>
+  <h2>But we still struggle to get rid of them!</h2>
+  <p>Yes.</p>
 </main>
